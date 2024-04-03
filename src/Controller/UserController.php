83ca778fc700982a43c796882e122f3d90f4e1c5
Clement Haytham <116069043+hxthm@users.noreply.github.com>
2024-04-03 14:05:41 +0200

    update register and login

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,97 +1,98 @@
 <?php
 
-    namespace App\Controller;
+namespace App\Controller;
 
-    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-    use Symfony\Component\HttpFoundation\JsonResponse;
-    use Symfony\Component\Routing\Attribute\Route;
-    use Symfony\Component\HttpFoundation\Request;
-    use Doctrine\ORM\EntityManagerInterface;
-    use App\Repository\UserRepository;
-    use Symfony\Component\Serializer\SerializerInterface;
-    use Symfony\Component\HttpFoundation\Response;
-    use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-    use App\Entity\User;
-    use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
-    use Symfony\Component\Security\Core\Exception\BadCredentialsException;
-    use Symfony\Component\Cache\Adapter\FilesystemAdapter;
-    use Psr\Cache\CacheItemPoolInterface;
-    use Symfony\Component\Cache\Adapter\AdapterInterface;
-    use Symfony\Component\Cache\CacheItem;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\UserRepository;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Entity\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Psr\Cache\CacheItemPoolInterface;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
+use Symfony\Component\Cache\CacheItem;
 
 
-    class UserController extends AbstractController
+class UserController extends AbstractController
+{
+
+    private $userRepository;
+    private $passwordEncoder;
+    private $entityManager;
+    private $serializer;
+    private $jwtManager;
+
+    public function __construct(
+        UserRepository $userRepository,
+        EntityManagerInterface $entityManager,
+        SerializerInterface $serializer,
+        UserPasswordHasherInterface $passwordEncoder,
+        JWTTokenManagerInterface $jwtManager,
+    ) {
+        $this->userRepository = $userRepository;
+        $this->entityManager = $entityManager;
+        $this->serializer = $serializer;
+        $this->passwordEncoder = $passwordEncoder;
+        $this->jwtManager = $jwtManager;
+    }
+
+    #[Route('/register', name: 'create_user', methods: 'POST')]
+    public function createUser(Request $request): JsonResponse
     {
+        try {
+            //$idUser = $request->request->get('idUser');
+            $firstName = $request->request->get('firstName');
+            $lastName = $request->request->get('lastName');
+            $email = $request->request->get('email');
+            $password = $request->request->get('password');
+            $birth = $request->request->get('birth');
+            $tel = $request->request->has('tel') ? $request->request->get('tel') : null;
+            $sexe = $request->request->has('sexe') ? $request->request->get('sexe') : null;
 
-        private $userRepository;
-        private $passwordEncoder;
-        private $entityManager;
-        private $serializer;
-        private $jwtManager;
+            if ($firstName === null || $lastName === null || $email === null || $password === null || $birth === null) {
+                return new JsonResponse([
+                    'error' => true,
+                    'message' => 'Une ou plusieurs données obligatoires sont manquantes',
+                ], JsonResponse::HTTP_BAD_REQUEST);
+            }
 
-        public function __construct(
-            UserRepository $userRepository,
-            EntityManagerInterface $entityManager,
-            SerializerInterface $serializer,
-            UserPasswordHasherInterface $passwordEncoder,
-            JWTTokenManagerInterface $jwtManager,
-        ) {
-            $this->userRepository = $userRepository;
-            $this->entityManager = $entityManager;
-            $this->serializer = $serializer;
-            $this->passwordEncoder = $passwordEncoder;
-            $this->jwtManager = $jwtManager;
-        }
+            if ( //strlen($idUser) > 90 || 
+                strlen($firstName) > 55 || strlen($lastName) > 55 || strlen($email) > 80 || strlen($password) > 90 || (strlen($tel) > 0 && strlen($tel) > 15) || (strlen($sexe) > 0 && strlen($sexe) > 30)
+            ) {
+                return new JsonResponse([
+                    'error' => true,
+                    'message' => 'Une ou plusieurs données sont éronnées (Trop longues)',
+                    'data' => [
+                        //'idUser' => $idUser,
+                        'firstName' => $firstName,
+                        'lastName' => $lastName,
+                        'email' => $email,
+                        'password' => $password,
+                        'tel' => $tel,
+                        'sexe' => $sexe,
+                    ],
+                ], JsonResponse::HTTP_CONFLICT);
+            }
 
-        #[Route('/register', name: 'create_user', methods: 'POST')]
-        public function createUser(Request $request): JsonResponse
-        {
-            try{
-                $idUser = $request->request->get('idUser');
-                $firstName = $request->request->get('firstName');
-                $lastName = $request->request->get('lastName');
-                $email = $request->request->get('email');
-                $password = $request->request->get('password');
-                $tel = $request->request->get('tel');
-                $birth = $request->request->get('birth');
-                $sexe = $request->request->get('sexe');
+            $emailRegex = '/^\S+@\S+\.\S+$/';
+            if (!preg_match($emailRegex, $email)) {
+                return new JsonResponse([
+                    'error' => true,
+                    'message' => 'Une ou plusieurs données sont éronnées',
+                    'data' => [
+                        'email' => $email,
+                    ],
+                ], JsonResponse::HTTP_CONFLICT);
+            }
 
-                if ($firstName === null || $lastName === null || $email === null || $password === null || $birth === null) {
-                    return new JsonResponse([
-                            'error' => true,
-                            'message' => 'Une ou plusieurs données obligatoires sont manquantes',
-                        ], JsonResponse::HTTP_BAD_REQUEST);
-                }
-
-                if (strlen($idUser) > 90 || strlen($firstName) > 55 || strlen($lastName) > 55 || strlen($email) > 80 || strlen($password) > 90 || strlen($tel) > 15 || strlen($sexe) > 30) {
-                    return new JsonResponse([
-                        'error' => true,
-                        'message' => 'Une ou plusieurs données sont éronnées (Trop longues)',
-                        'data' => [
-                            'idUser' => $idUser,
-                            'firstName' => $firstName,
-                            'lastName' => $lastName,
-                            'email' => $email,
-                            'password' => $password,
-                            'tel' => $tel,
-                            'sexe' => $sexe,
-                        ],
-                    ], JsonResponse::HTTP_CONFLICT);
-                }
-
-
-
-                $emailRegex = '/^\S+@\S+\.\S+$/';
-                if (!preg_match($emailRegex, $email)) {
-                    return new JsonResponse([
-                        'error' => true,
-                        'message' => 'Une ou plusieurs données sont éronnées',
-                        'data' => [
-                            'email' => $email,
-                        ],
-                    ], JsonResponse::HTTP_CONFLICT);
-                }
-
+            if ($tel !== null) {
                 $phoneRegex = '/^\d{10}$/';
                 if (!preg_match($phoneRegex, $tel)) {
                     return new JsonResponse([
@@ -102,60 +103,60 @@
                         ],
                     ], JsonResponse::HTTP_CONFLICT);
                 }
-
-                $date = new \DateTime($birth);
-                $now = new \DateTime();
-                $age = $now->diff($date)->y;
-                if ($age < 12) {
-                    return new JsonResponse([
-                        'error' => true,
-                        'message' => "L'âge de l'utilisateur ne permet pas (12 ans)",
-                    ], JsonResponse::HTTP_NOT_ACCEPTABLE);
-                }
-
-                $existingUser = $this->userRepository->findOneBy(['email' => $email]);
-                if ($existingUser) {
-                return new JsonResponse([
-                        'error' => true,
-                        'message' => 'Un compte utilisant cet adresse mail est déjà enregistré',
-                    ], JsonResponse::HTTP_CONFLICT);
-                }
-
-
-                $user = new User();
-                $user->setIdUser($idUser);
-                $user->setFirstName($firstName);
-                $user->setLastName($lastName);
-                $user->setEmail($email);
-                $user->setPassword($this->passwordEncoder->hashPassword($user, $password));
-                $user->setTel($tel);
-                $user->setSexe($sexe);
-                $user->setBirth(new \DateTime($birth));
-                $user->setCreateAt(new \DateTimeImmutable());
-                $user->setUpdateAt(new \DateTimeImmutable());
-
-
-                $this->entityManager->persist($user);
-                $this->entityManager->flush();
-                
-                $serealizedUser = $this->serializer->serialize($user, 'json' , ['ignored_attributes' => ['id','password', 'idUser', 'artist']]);
-                $userArray = json_decode($serealizedUser, true);
-                $userArray['birth'] = $user->getBirth()->format('Y-m-d');
-                $user = $this->userRepository->findOneBy(['idUser' => $idUser]);
-                
-                return $this->json([
-                    'error' => 'false',
-                    'message' => "L'utilisateur a bien été créé avec succès.",
-                    'user' => $userArray,
-                ]);
-            } catch (\Exception $e) {
-                return new JsonResponse([
-                    'error' => 'Error: ' . $e->getMessage(),
-                ], JsonResponse::HTTP_NOT_FOUND);
             }
-        }
 
-        #[Route('/login', name: 'login_user', methods: 'POST')]
+            $date = new \DateTime($birth);
+            $now = new \DateTime();
+            $age = $now->diff($date)->y;
+            if ($age < 12) {
+                return new JsonResponse([
+                    'error' => true,
+                    'message' => "L'âge de l'utilisateur ne permet pas (12 ans)",
+                ], JsonResponse::HTTP_NOT_ACCEPTABLE);
+            }
+
+            $existingUser = $this->userRepository->findOneBy(['email' => $email]);
+            if ($existingUser) {
+                return new JsonResponse([
+                    'error' => true,
+                    'message' => 'Un compte utilisant cette adresse mail est déjà enregistré',
+                ], JsonResponse::HTTP_CONFLICT);
+            }
+
+            $user = new User();
+            //$user->setIdUser($idUser);
+            $user->setFirstName($firstName);
+            $user->setLastName($lastName);
+            $user->setEmail($email);
+            $user->setPassword($this->passwordEncoder->hashPassword($user, $password));
+            $user->setTel($tel);
+            $user->setSexe($sexe);
+            $user->setBirth(new \DateTime($birth));
+            $user->setCreateAt(new \DateTimeImmutable());
+            $user->setUpdateAt(new \DateTimeImmutable());
+
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+
+            $serializedUser = $this->serializer->serialize($user, 'json', ['ignored_attributes' => ['id', 'password', 'idUser', 'artist', 'salt', 'username', 'userIdentifier', 'roles']]);
+            $userArray = json_decode($serializedUser, true);
+            $userArray['birth'] = $user->getBirth()->format('Y-m-d');
+            $user = $this->userRepository->findOneBy(['email' => $email]);
+
+            return $this->json([
+                'error' => false,
+                'message' => "L'utilisateur a bien été créé avec succès.",
+                'user' => $userArray,
+            ]);
+        } catch (\Exception $e) {
+            return new JsonResponse([
+                'error' => 'Error: ' . $e->getMessage(),
+            ], JsonResponse::HTTP_NOT_FOUND);
+        }
+    }
+
+
+    #[Route('/login', name: 'login_user', methods: 'POST')]
     public function login(Request $request, CacheItemPoolInterface $cache, JWTTokenManagerInterface $jwtManager): JsonResponse
     {
         try {
@@ -221,15 +222,28 @@
 
             $cache->deleteItem($cacheKeyAttempts);
 
-            $serializedUser = $this->serializer->serialize($user, 'json' , ['ignored_attributes' => ['id','password', 'idUser', 'roles','salt','username','userIdentifier','updateAt']]);
-            $userArray = json_decode($serializedUser, true);
-            $userArray['birth'] = $user->getBirth()->format('d-m-Y');
+            $artist = $user->getArtist();
+
+            $userArray = [
+                'firstName' => $user->getFirstName(),
+                'lastName' => $user->getLastName(),
+                'email' => $user->getEmail(),
+                'tel' => $user->getTel(),
+                'sexe' => $user->getSexe(),
+                'birth' => $user->getBirth()->format('d-m-Y'),
+                'createAt' => $user->getCreateAt()->format('Y-m-d\TH:i:sP'),
+                'artist' => $artist ? [
+                    'label' => $artist->getLabel(),
+                    'description' => $artist->getDescription(),
+                    'fullname' => $artist->getFullname(),
+                ] : null,
+            ];
 
             $token = $jwtManager->create($user);
 
             return $this->json([
                 'error' => 'false',
-                'message' => "L'utilisateur a été authentifié succès",
+                'message' => "L'utilisateur a été authentifié avec succès",
                 'user' => $userArray,
                 'token' => $token,
             ]);
@@ -239,6 +253,7 @@
             ], JsonResponse::HTTP_NOT_FOUND);
         }
     }
+
 
 
     /*     #[Route('/user/edit', name: 'edit_user', methods: 'PUT')]
@@ -294,38 +309,26 @@
             }
         }*/
 
-        #[Route('/user', name: 'delete_user', methods: ['DELETE'])]
-        public function deleteUser(): JsonResponse
-        {
-            try {
+    #[Route('/user', name: 'delete_user', methods: ['DELETE'])]
+    public function deleteUser(): JsonResponse
+    {
+        try {
 
-                $currentUser = $this->getUser()->getUserIdentifier();
+            $currentUser = $this->getUser()->getUserIdentifier();
 
-                $user = $this->userRepository->findOneBy(['email' => $currentUser]);
-                
-                $this->entityManager->remove($user);
-                $this->entityManager->flush();
+            $user = $this->userRepository->findOneBy(['email' => $currentUser]);
 
-                return $this->json([
-                    'error' => 'false',
-                    'message' => 'Votre compte a été supprimé avec succès',
-                ]);
+            $this->entityManager->remove($user);
+            $this->entityManager->flush();
 
-                
-            } catch (\Exception $e) {
-                return new JsonResponse([
-                    'error' => 'Erreur: ' . $e->getMessage(),
-                ], JsonResponse::HTTP_NOT_FOUND);
-            }
-
-
+            return $this->json([
+                'error' => 'false',
+                'message' => 'Votre compte a été supprimé avec succès',
+            ]);
+        } catch (\Exception $e) {
+            return new JsonResponse([
+                'error' => 'Erreur: ' . $e->getMessage(),
+            ], JsonResponse::HTTP_NOT_FOUND);
         }
-
-        
-
-        
-
     }
-
-
-
+}
